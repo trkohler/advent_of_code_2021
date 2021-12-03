7f2ffb019a27642dd6000920bd030d54ddc79a2a
t.kohler <t.kohler@smartweb.com.ua>
2021-12-03 13:32:32 +0200

    second day starts

---
+++ day1/first/solution.py
@@ -0,0 +1,16 @@
+
+with open("day1/first/input_for_first_puzzle.txt", "r") as input_file:
+    lines = input_file.readlines()
+    lines = [int(el) for el in lines]
+
+
+def count_increase(measurement_list: list) -> int:
+    count = 0
+    for i in range(1, len(measurement_list)):
+        if measurement_list[i] > measurement_list[i-1]:
+            count += 1
+    return count
+
+if __name__ == "__main__":
+    count = count_increase(lines)
+    print(count)
---
+++ day1/second/solution.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+with open("day1/second/input_for_second_puzzle.txt", "r") as input_file:
+    lines = input_file.readlines()
+    lines = [int(el) for el in lines]
+    
+def count_increase_with_window(measurements: list, window_size: int) -> int:
+    d = deque(maxlen=window_size)
+    previous_sum = None
+    count = 0
+    
+    for i in range(len(measurements)):
+        d.append(measurements[i])
+        current_deque_sum = sum(d)
+        
+        if len(d) == window_size:
+            if previous_sum and current_deque_sum > previous_sum:
+                count += 1
+            previous_sum = current_deque_sum
+        
+    
+    return count
+
+if __name__ == "__main__":
+    count = count_increase_with_window(lines, 3)
+    print(count)
